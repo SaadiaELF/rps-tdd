4ec2dc06b484fbce1d8870954094378285f8fef1
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-10 22:09:23 +0100

    Fourth test - paper vs. scissors

--- index.test.js
+++ index.test.js
@@ -27,4 +27,12 @@ describe("rock, paper, scissors", () => {
 
     expect(result).toBe("left");
   });
+  it("should say right wins for paper vs. scissors", () => {
+    const left = "paper";
+    const right = "scissors";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("right");
+  });
 });
